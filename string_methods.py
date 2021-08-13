ae12509ce7e6bf0544fef9ca92b15bd6897f1787
Navian <primeguyy@gmail.com>
2021-08-13 10:40:59 +0000

    some string methods

---
+++ string_methods.py
@@ -0,0 +1,18 @@
+firstName = "Dean"
+midName = "van"
+lastName = "Thompson"
+multiplyString = "good "
+
+print(firstName, lastName)
+print(firstName.isprintable())
+print(firstName.upper())
+print(midName.capitalize())
+print(firstName.lower())
+print(len(lastName))
+print(lastName.find("s"))
+print(midName.isdigit())
+print(midName.isalpha())
+print(lastName.count("o"))
+print(lastName.replace("o", "i"))
+# repeat or multiply string
+print(multiplyString * 5)
