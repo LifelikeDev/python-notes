81bb98bc270c56f8f2811fbce17f37841b5b3bfb
Navian <primeguyy@gmail.com>
2021-08-13 14:50:58 +0000

    user input

---
+++ user_input.py
@@ -0,0 +1,16 @@
+# print("Hello there")
+#
+# # use the input method (Scanner class in Java) for user input
+# username = input("What is your name? ")
+# ####### OR
+# # username = input("""What is your name?
+# # """)
+# print("Welcome", username + "!")
+
+print("Hello there...")
+name = input("""What is your name?
+""")
+favourite_colour = input("""What is your favourite colour?
+""")
+print("Your name is", name, "and your favourite colour is", favourite_colour)
+
