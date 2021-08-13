5e91330bdb1840338f9eaf9d7f5041684b3a8f1c
Navian <primeguyy@gmail.com>
2021-08-13 10:17:09 +0000

    Python basics

---
+++ variables.py
@@ -0,0 +1,14 @@
+### FOUR Basic Data types in Python
+
+# String
+name = "Joan"
+# Int
+age = 18
+# Float
+height = 5.5
+# Boolean
+isAMale = False
+
+print("Hello " + name)
+print("Your age is " + str(age))
+print("You are " + str(height) + " m tall")
