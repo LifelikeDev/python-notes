fc9122f564ef0a3a53ca582990bae2b1d053d937
Navian <primeguyy@gmail.com>
2021-08-13 10:27:04 +0000

    assignments

---
+++ assignment.py
@@ -0,0 +1,18 @@
+# Standard assignment
+name = "Mike"
+age = 43
+height = 56
+isRich = True
+
+print(name)
+print(age)
+print(isRich)
+
+print()
+# Multiple Assignments
+nickName = guideName = otherName = "Dallor-O"
+
+print(nickName)
+print(guideName)
+print(otherName)
+
