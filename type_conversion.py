c8e9fafe152065b029f6636b18fd411ecb66c16c
Navian <primeguyy@gmail.com>
2021-08-13 15:15:13 +0000

    type casting / type conversion

---
+++ type_conversion.py
@@ -0,0 +1,18 @@
+### add a type cast by adding the data type with the value to be cast in parenthesis
+
+# name = input("What is your name? ")
+# birth_year = input("In which year were your born? ")
+# print(type (birth_year))
+# # type casting, since birth_year is a string
+# age = 2021 - int(birth_year)
+# print("You are", age, "years old")
+
+# figure = "21"
+# print(18, type(int(figure)))
+
+# RENDER USER WEIGHT IN KILOGRAMS
+weight_in_lbs = input("Enter your weight in pounds(lbs) ")
+weight_in_kg = int(weight_in_lbs) * 0.45
+
+print("Your weigh", weight_in_kg, "kg")
+print("Is that " "too much " "weight?")
