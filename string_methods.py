bd47f8e100c02941ae0226fa2d3c2f56510c0d72
Navian <primeguyy@gmail.com>
2021-08-13 10:44:08 +0000

    more string methods

--- string_methods.py
+++ string_methods.py
@@ -1,18 +1,18 @@
-firstName = "Dean"
-midName = "van"
-lastName = "Thompson"
-multiplyString = "good "
+first_name = "Dean"
+mid_name = "van"
+last_name = "Thompson"
+multiply_string = "good "
 
-print(firstName, lastName)
-print(firstName.isprintable())
-print(firstName.upper())
-print(midName.capitalize())
-print(firstName.lower())
-print(len(lastName))
-print(lastName.find("s"))
-print(midName.isdigit())
-print(midName.isalpha())
-print(lastName.count("o"))
-print(lastName.replace("o", "i"))
+print(first_name, last_name)
+print(first_name.isprintable())
+print(first_name.upper())
+print(mid_name.capitalize())
+print(first_name.lower())
+print(len(last_name))
+print(last_name.find("s"))
+print(mid_name.isdigit())
+print(mid_name.isalpha())
+print(last_name.count("o"))
+print(last_name.replace("o", "i"))
 # repeat or multiply string
-print(multiplyString * 5)
+print(multiply_string * 5)
