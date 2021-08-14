f0f8925eb06a302501fbd41b31bc6cd3e1a52cdc
Navian <primeguyy@gmail.com>
2021-08-14 15:13:53 +0000

    conditional loops

---
+++ conditional_loops.py
@@ -0,0 +1,15 @@
+user_age = int(input("""Hi there...
+Enter your age: """))
+
+if user_age <= 0:
+    print("Invalid input! Enter a valid age")
+elif 0 < user_age <= 12:
+    print("You're a baby")
+elif 12 < user_age <= 19:
+    print("You're a teenager")
+elif 19 < user_age <= 40:
+    print("You're a young adult")
+elif 40 < user_age <= 60:
+    print("You're an old adult")
+else:
+    print("You're a grannie")
