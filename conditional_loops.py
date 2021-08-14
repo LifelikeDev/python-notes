7cefd2c48e9b46598e1878e8de73abdf2f768803
Navian <primeguyy@gmail.com>
2021-08-14 18:42:15 +0000

    conditional loops (edited)

--- conditional_loops.py
+++ conditional_loops.py
@@ -1,15 +1,37 @@
-user_age = int(input("""Hi there...
-Enter your age: """))
+## IF_ELSE
 
-if user_age <= 0:
-    print("Invalid input! Enter a valid age")
-elif 0 < user_age <= 12:
-    print("You're a baby")
-elif 12 < user_age <= 19:
-    print("You're a teenager")
-elif 19 < user_age <= 40:
-    print("You're a young adult")
-elif 40 < user_age <= 60:
-    print("You're an old adult")
-else:
-    print("You're a grannie")
+# user_age = int(input("""Hi there...
+# Enter your age: """))
+#
+# if user_age <= 0:
+#     print("Invalid input! Enter a valid age")
+# elif 0 < user_age <= 12:
+#     print("You're a baby")
+# elif 12 < user_age <= 19:
+#     print("You're a teenager")
+# elif 19 < user_age <= 40:
+#     print("You're a young adult")
+# elif 40 < user_age <= 60:
+#     print("You're an old adult")
+# else:
+#     print("You're a grannie")
+#
+# print()
+
+### WHILE
+
+# numb = 10
+# i = 0
+
+# while numb > i:
+#     print(f"loop {numb}")
+#     numb = numb - 1
+# print("Process finished...")
+
+
+
+
+
+
+
+
