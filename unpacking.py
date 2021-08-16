5430beb10b7700b480fcf3b0f66374f7652e4929
Navian <primeguyy@gmail.com>
2021-08-16 19:53:56 +0000

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,31 @@
+# Storing elements in a list / tuple in variables for easy accessibility
+
+numbers = (2, 3, 4, 5)
+a = numbers[0]
+b = numbers[1]
+c = numbers[2]
+d = numbers[3]
+print(a)
+print(b)
+print(c)
+print(d)
+print()
+
+# Unpacking makes the above easier
+
+j, k, l, m = numbers
+print(j)
+print(k)
+print(l)
+print(m)
+print()
+
+# In lists,
+milestones = [200, 400, 800, 1200, 1600]
+
+i, ii, iii, iv, v = milestones
+print(i)
+print(ii)
+print(iii)
+print(iv)
+print(v)
